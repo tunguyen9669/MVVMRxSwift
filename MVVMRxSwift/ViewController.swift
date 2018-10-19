c06110527692c2dadc5dde041c0132d35c271fab
admin <tunguyen090896@gmail.com>
2018-10-19 10:41:51 +0700

    Initial Commit

---
+++ MVVMRxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMRxSwift
+//
+//  Created by admin on 10/19/18.
+//  Copyright © 2018 admin.dinhtu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
