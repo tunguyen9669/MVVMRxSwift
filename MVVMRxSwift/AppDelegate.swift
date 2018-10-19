bfa3e68fda011358702649811001ab6b35e3fa65
admin <tunguyen090896@gmail.com>
2018-10-19 11:05:17 +0700

    second commit

--- MVVMRxSwift/AppDelegate.swift
+++ MVVMRxSwift/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let rootView = SuperStarViewController(nibName: "SuperStarViewController", bundle: nil)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = rootView
+        window?.makeKeyAndVisible()
         return true
     }
 
